fc4d52a3d8ec2f217c761b1d718081170f964277
taizan-hokuto <55448286+taizan-hokuto@users.noreply.github.com>
2020-03-26 22:41:01 +0900

    Increment version

--- pytblocklib/__init__.py
+++ pytblocklib/__init__.py
@@ -2,12 +2,12 @@
 a python library for youtube blocking functions without using yt api, Selenium, or BeautifulSoup.
 """
 __copyright__    = 'Copyright (C) 2020 taizan-hokuto'
-__version__      = '0.0.4'
+__version__      = '0.0.5'
 __license__      = 'GNU GENERAL PUBLIC LICENSE Version 3'
 __author__       = 'taizan-hokuto'
 __author_email__ = '55448286+taizan-hokuto@users.noreply.github.com'
 __url__          = 'https://github.com/taizan-hokuto/pytblocklib'
 
-RELEASE = False
+RELEASE = True
 
 from .watcher import Watcher
