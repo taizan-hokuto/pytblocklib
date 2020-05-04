038639f03cbcba3b5901709976d13e3f7bccd28e
taizan-hokuto <55448286+taizan-hokuto@users.noreply.github.com>
2020-05-04 20:29:07 +0900

    Make it possible to display amount string

--- pytblocklib/chat/processors/default/renderer/base.py
+++ pytblocklib/chat/processors/default/renderer/base.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 class BaseRenderer:
     def __init__(self, item, chattype):
         self.renderer = list(item.values())[0]
@@ -16,30 +17,31 @@ class BaseRenderer:
         else:
             self.elapsed = ""
         self.datetime = self.get_datetime(timestampUsec)
-        self.message ,self.message_ex = self.get_message(self.renderer)
-        self.id =  self.renderer.get('id')
+        self.message, self.message_ex = self.get_message(self.renderer)
+        self.id = self.renderer.get('id')
         self.bg_color = 0
+        self.txt_color = 0
+        self.amount_string = ''
         self.params = self.renderer["contextMenuEndpoint"]["liveChatItemContextMenuEndpoint"]["params"]
 
     def get_authordetails(self):
         self.author_badge = ""
-        (self.is_verified, 
-        self.is_owner, 
-        self.is_member, 
+        (self.is_verified,
+        self.is_owner,
+        self.is_member,
         self.is_moderator) = (
             self.get_badges(self.renderer)
         )
         self.author_id = self.renderer.get("authorExternalChannelId")
         self.author_channel = "http://www.youtube.com/channel/"+self.author_id
-        self.author_name      = self.renderer["authorName"]["simpleText"]
-        self.author_image= self.renderer["authorPhoto"]["thumbnails"][1]["url"] 
-        
+        self.author_name = self.renderer["authorName"]["simpleText"]
+        self.author_image = self.renderer["authorPhoto"]["thumbnails"][1]["url"] 
 
-    def get_message(self,renderer):
+    def get_message(self, renderer):
         message = ''
         message_ex = []
         if renderer.get("message"):
-            runs=renderer["message"].get("runs")
+            runs = renderer["message"].get("runs")
             if runs:
                 for r in runs:
                     if r:
@@ -51,17 +53,16 @@ class BaseRenderer:
                             message_ex.append(r.get('text',''))
         return message, message_ex
 
-
-    def get_badges(self,renderer):
+    def get_badges(self, renderer):
         is_verified = False
         is_owner = False
         is_member = False
         is_moderator = False
-        badges=renderer.get("authorBadges")
+        badges = renderer.get("authorBadges")
         if badges:
             for badge in badges:
                 if badge["liveChatAuthorBadgeRenderer"].get("icon"):
-                    author_type  = badge["liveChatAuthorBadgeRenderer"]["icon"]["iconType"]
+                    author_type = badge["liveChatAuthorBadgeRenderer"]["icon"]["iconType"]
                     if author_type == 'VERIFIED':
                         is_verified = True
                     if author_type == 'OWNER':
@@ -72,16 +73,13 @@ class BaseRenderer:
                     is_member = True
                     self.get_badgeurl(badge)
         return is_verified, is_owner, is_member, is_moderator
-    
 
-    def get_badgeurl(self,badge):
+    def get_badgeurl(self, badge):
         self.author_badge = badge["liveChatAuthorBadgeRenderer"]["customThumbnail"]["thumbnails"][0]["url"]
 
-
-    def get_datetime(self,timestamp):
+    def get_datetime(self, timestamp):
         dt = datetime.fromtimestamp(timestamp/1000000)
         return dt.strftime('%Y-%m-%d %H:%M:%S')
-
 
     def cleanup(self):
         self.renderer = None
--- pytblocklib/chat/processors/default/renderer/paidmessage.py
+++ pytblocklib/chat/processors/default/renderer/paidmessage.py
@@ -1,8 +1,12 @@
-import re
 from .base import BaseRenderer
+
 
 class LiveChatPaidMessageRenderer(BaseRenderer):
     def __init__(self, item):
         super().__init__(item, "super_chat")
 
-            
+    def get_snippet(self):
+        super().get_snippet()
+        self.amount_string = self.renderer["purchaseAmountText"]["simpleText"]
+        self.bg_color = self.renderer.get("bodyBackgroundColor", 0)
+        self.txt_color = self.renderer.get("bodyTextColor", 0)
